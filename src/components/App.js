9b9b245ead8d9f1308113dedf6cb5a5133c9749f
Aprisyta <aprisyta10@gmail.com>
2018-01-20 22:32:19 +0530

    Basic redux framework

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function App(){
   return <div className="App">  Hello  </div>
 }
-class App extends Component {
+// class App extends Component {
 //   render() {
 //     return (
 //       <div className="App">
