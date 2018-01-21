c29bb8369a0e6de71d2fe8b8f89507bc83add61e
Aprisyta <aprisyta10@gmail.com>
2018-01-21 20:05:47 +0530

    Using redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { addRecipe } from '../actions'
 
-function App(){
-  return <div className="App">  Hello  </div>
+class App extends Component {
+
+  state = {
+    calendar: null
+  }
+
+  componentDidMount () {
+    const { store } = this.props
+
+    store.subscribe(() => {
+      this.setState(() => {
+        calendar: store.getState()
+      })
+    })
+  }
+  submitFood = () => {
+    this.props.store.dispatch(addRecipe({
+      day: 'monday',
+      meal: 'breakfast',
+      recipe: {
+        label: this.input.value
+      }
+    }))
+
+    this.input.value = ''
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type='text'
+          ref={(input) => this.input = input}
+          placeholder="Monday's Breakfast"
+        />
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
+      </div>
+    );
+  }
 }
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         Hello
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
